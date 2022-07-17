6219e2ad751b51aee4d3e85ca92d849bf8763188
Andrea Montes <mamontesp@gmail.com>
2022-07-17 17:53:07 -0500

    Pit 07 updating models (#8)
    * lku model update
    
    * Adding queries for analytics

--- models/analytics/analytics_comcel_coverage.sql
+++ models/analytics/analytics_comcel_coverage.sql
@@ -2,6 +2,6 @@
 
 select * 
 from 
-dwh_rpt.rpt_services_by_operator 
+{{ref('rpt_services_by_operator')}}
 where company_id = '800153993'
 order by year, quarter
--- models/rpt/rpt_services_by_operator.sql
+++ models/rpt/rpt_services_by_operator.sql
@@ -5,9 +5,9 @@ select
   sc.company_name,
   {{ dbt_utils.pivot(
       column='service',
-      values=dbt_utils.get_column_values(ref('lku_services_by_operator'), 'service')
+      values=dbt_utils.get_column_values(ref('lku_service_all_operator'), 'service')
   ) }}
-from {{ ref('lku_services_by_operator') }} so
+from {{ ref('lku_service_all_operator') }} so
 join {{ ref ('stg_companies')}} sc
 on so.company_id = sc.company_id
 group by 1,2,3,4
