7ef1da531b13319fb8eee55c7a084e8db29ad65f
Andrea Montes <mamontesp@gmail.com>
2022-07-17 15:50:29 -0500

    Adding some critical commands + updating lku_capacity_internet_fixed_by_operator (#7)

--- models/lku/lku_capacity_internet_fixed_by_operator.sql
+++ models/lku/lku_capacity_internet_fixed_by_operator.sql
@@ -2,7 +2,8 @@ select
 distinct company_id,
 year, 
 quarter,
-'internet_fixed' as service
+'internet' as service,
+'mobile' as service_type
 from  
 {{ ref('base_capacity_internet_fixed_access_points') }}
 group by 1,2,3
